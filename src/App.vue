ade10fb48c835165d9fac5e717217bbf98c95d56
PearThanchat <thanchat.jan@gmail.com>
2024-07-17 19:44:05 +0700

    start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+  </nav> -->
+  <RouterView />
+  <!-- <h1 class="text-3xl font-bold underline">Hello world!</h1> -->
+</template>
